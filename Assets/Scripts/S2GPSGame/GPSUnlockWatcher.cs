901cdef8895f853afd4cab6d1ffeab811495a01c
Alan Yu <alanrain205@gmail.com>
2026-03-28 14:38:46 -0600

    Fix Invisible CLue Bug

--- Assets/Scripts/S2GPSGame/GPSUnlockWatcher.cs
+++ Assets/Scripts/S2GPSGame/GPSUnlockWatcher.cs
@@ -1,12 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Reflection;
 using UnityEngine;
 
 public class GPSUnlockWatcher : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private Component inventorySource;
+    [SerializeField] private ClueJournal clueJournal;
     [SerializeField] private ClueDefinition gpsClue;
     [SerializeField] private GPSTrackerMissionStarter missionStarter;
 
@@ -16,6 +14,12 @@ public class GPSUnlockWatcher : MonoBehaviour
 
     private bool hasTriggered = false;
     private Coroutine watchRoutine;
+
+    private void Awake()
+    {
+        if (clueJournal == null)
+            clueJournal = ClueJournal.Instance;
+    }
 
     private void OnEnable()
     {
@@ -33,11 +37,11 @@ public class GPSUnlockWatcher : MonoBehaviour
         while (true)
         {
             if ((!triggerOnlyOnce || !hasTriggered) &&
-                inventorySource != null &&
+                clueJournal != null &&
                 gpsClue != null &&
                 missionStarter != null)
             {
-                if (PlayerHasGPSClue())
+                if (JournalHasGPSClue())
                 {
                     missionStarter.StartTrackingMission();
                     hasTriggered = true;
@@ -49,205 +53,11 @@ public class GPSUnlockWatcher : MonoBehaviour
         }
     }
 
-    private bool PlayerHasGPSClue()
+    private bool JournalHasGPSClue()
     {
-        object source = inventorySource;
-        string gpsId = GetClueId(gpsClue);
+        if (clueJournal == null || gpsClue == null) return false;
+        if (string.IsNullOrWhiteSpace(gpsClue.id)) return false;
 
-        string[] methodNames =
-        {
-            "HasClue",
-            "ContainsClue",
-            "HasItem",
-            "ContainsItem",
-            "Contains",
-            "Has"
-        };
-
-        foreach (string methodName in methodNames)
-        {
-            MethodInfo method = source.GetType().GetMethod(
-                methodName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-            );
-
-            if (method == null) continue;
-
-            ParameterInfo[] ps = method.GetParameters();
-            if (ps.Length != 1) continue;
-
-            try
-            {
-                if (ps[0].ParameterType == typeof(ClueDefinition))
-                {
-                    object result = method.Invoke(source, new object[] { gpsClue });
-                    if (result is bool b && b) return true;
-                }
-
-                if (ps[0].ParameterType == typeof(string) && !string.IsNullOrEmpty(gpsId))
-                {
-                    object result = method.Invoke(source, new object[] { gpsId });
-                    if (result is bool b && b) return true;
-                }
-            }
-            catch { }
-        }
-
-        string[] memberNames =
-        {
-            "clues",
-            "items",
-            "inventory",
-            "collectedClues",
-            "ownedClues",
-            "definitions"
-        };
-
-        foreach (string memberName in memberNames)
-        {
-            FieldInfo field = source.GetType().GetField(
-                memberName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-            );
-
-            if (field != null)
-            {
-                object value = field.GetValue(source);
-                if (CollectionContainsGPS(value)) return true;
-            }
-
-            PropertyInfo prop = source.GetType().GetProperty(
-                memberName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-            );
-
-            if (prop != null && prop.CanRead)
-            {
-                object value = prop.GetValue(source);
-                if (CollectionContainsGPS(value)) return true;
-            }
-        }
-
-        return false;
-    }
-
-    private bool CollectionContainsGPS(object collectionObj)
-    {
-        if (collectionObj == null) return false;
-
-        if (collectionObj is IEnumerable enumerable)
-        {
-            foreach (object item in enumerable)
-            {
-                if (item == null) continue;
-
-                if (item is ClueDefinition clueDef)
-                {
-                    if (clueDef == gpsClue) return true;
-
-                    string itemId = GetClueId(clueDef);
-                    string gpsId = GetClueId(gpsClue);
-
-                    if (!string.IsNullOrEmpty(itemId) && itemId == gpsId) return true;
-                }
-
-                if (TryMatchNestedItem(item)) return true;
-            }
-        }
-
-        return false;
-    }
-
-    private bool TryMatchNestedItem(object item)
-    {
-        System.Type t = item.GetType();
-        string gpsId = GetClueId(gpsClue);
-
-        string[] idNames = { "id", "Id" };
-        foreach (string idName in idNames)
-        {
-            FieldInfo f = t.GetField(idName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            if (f != null)
-            {
-                object value = f.GetValue(item);
-                if (value is string s && s == gpsId) return true;
-            }
-
-            PropertyInfo p = t.GetProperty(idName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            if (p != null && p.CanRead)
-            {
-                object value = p.GetValue(item);
-                if (value is string s && s == gpsId) return true;
-            }
-        }
-
-        string[] clueNames = { "clue", "definition", "clueDefinition" };
-        foreach (string clueName in clueNames)
-        {
-            FieldInfo f = t.GetField(clueName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            if (f != null)
-            {
-                object value = f.GetValue(item);
-                if (value is ClueDefinition clueDef)
-                {
-                    if (clueDef == gpsClue) return true;
-
-                    string itemId = GetClueId(clueDef);
-                    if (!string.IsNullOrEmpty(itemId) && itemId == gpsId) return true;
-                }
-            }
-
-            PropertyInfo p = t.GetProperty(clueName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            if (p != null && p.CanRead)
-            {
-                object value = p.GetValue(item);
-                if (value is ClueDefinition clueDef)
-                {
-                    if (clueDef == gpsClue) return true;
-
-                    string itemId = GetClueId(clueDef);
-                    if (!string.IsNullOrEmpty(itemId) && itemId == gpsId) return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private string GetClueId(ClueDefinition clue)
-    {
-        if (clue == null) return null;
-
-        System.Type t = clue.GetType();
-
-        FieldInfo idFieldLower = t.GetField("id", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-        if (idFieldLower != null)
-        {
-            object value = idFieldLower.GetValue(clue);
-            if (value is string s) return s;
-        }
-
-        FieldInfo idFieldUpper = t.GetField("Id", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-        if (idFieldUpper != null)
-        {
-            object value = idFieldUpper.GetValue(clue);
-            if (value is string s) return s;
-        }
-
-        PropertyInfo idPropLower = t.GetProperty("id", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-        if (idPropLower != null && idPropLower.CanRead)
-        {
-            object value = idPropLower.GetValue(clue);
-            if (value is string s) return s;
-        }
-
-        PropertyInfo idPropUpper = t.GetProperty("Id", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-        if (idPropUpper != null && idPropUpper.CanRead)
-        {
-            object value = idPropUpper.GetValue(clue);
-            if (value is string s) return s;
-        }
-
-        return null;
+        return clueJournal.HasClue(gpsClue.id);
     }
 }
