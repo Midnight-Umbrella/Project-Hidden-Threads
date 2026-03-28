b979d6756515dbc6328383615572e5fe8b4df6d7
Alan Yu <alanrain205@gmail.com>
2026-03-28 14:26:51 -0600

    Fix GPS Game Structure & add starter detect gps clue

--- Assets/Scripts/S2GPSGame/GPSTrackerMissionStarter.cs
+++ Assets/Scripts/S2GPSGame/GPSTrackerMissionStarter.cs
@@ -5,9 +5,21 @@ public class GPSTrackerMissionStarter : MonoBehaviour
     [SerializeField] private GPSTrackerController tracker;
     [SerializeField] private GPSClueTarget targetToTrack;
 
+    private bool hasStarted = false;
+
     public void StartTrackingMission()
     {
+        if (hasStarted) return;
         if (tracker == null || targetToTrack == null) return;
+
         tracker.StartTracking(targetToTrack);
+        hasStarted = true;
+
+        Debug.Log("GPS tracking mission started.");
+    }
+
+    public bool HasStarted()
+    {
+        return hasStarted;
     }
 }
