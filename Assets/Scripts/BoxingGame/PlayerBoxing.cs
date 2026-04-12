80be4bd4944bc147c8a281652edf5f8cd816de0b
IAmPatch01 <thomas.gibson01@outlook.com>
2026-04-12 17:27:09 -0600

    PowerPanel and Boxing Music Scripts

--- Assets/Scripts/BoxingGame/PlayerBoxing.cs
+++ Assets/Scripts/BoxingGame/PlayerBoxing.cs
@@ -7,18 +7,26 @@ using UnityEngine.UI;
 
 public class PlayerBoxing : MonoBehaviour
 {
-    public float speed;  //主角移动速度
+    public float speed;  //     贫  俣 
     [SerializeField]
-    private float maxLife;  //玩家最大生命值
-    public float currentLife;  //主角当前生命值
+    private float maxLife;  //          值
+    public float currentLife;  //   堑 前    值
     [SerializeField]
     private Animator ani;
     [SerializeField]
-    private GameObject enemy;  //敌人物体
-    private float boxingHitDistance=1.2f;  //判定击中的距离
-    private float damage;  //玩家的攻击力
+    private GameObject enemy;  //        
+    private float boxingHitDistance=1.2f;  // 卸    械木   
+    private float damage;  //  业墓     
     [SerializeField]
-    private Slider playerLifeSlider;  //玩家生命条
+    private Slider playerLifeSlider;  //         
+    [Header("Audio")]
+    [SerializeField] private AudioSource HarperAudioSource;
+    [SerializeField] private AudioClip PunchHitClip;
+    [SerializeField] private float HitVolume = 1f;
+    [SerializeField] private AudioClip PunchMissClip;
+    [SerializeField] private float MissVolume = 1f;
+
+
 
 
     private void Awake()
@@ -28,12 +36,12 @@ public class PlayerBoxing : MonoBehaviour
     }
     void Update()
     {
-        //AD键左右移动
+        //AD       贫 
         if (Input.GetKey(KeyCode.A))
         {
-            //向左移动
+            //     贫 
             transform.Translate(Vector3.left * speed * Time.deltaTime);
-            //设置动画
+            //   枚   
             if (!ani.GetBool("isRight"))
             {
                 ani.SetBool("isRight", true);
@@ -45,9 +53,9 @@ public class PlayerBoxing : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            //向右移动
+            //     贫 
             transform.Translate(Vector3.right * speed * Time.deltaTime);
-            //设置动画
+            //   枚   
             if (ani.GetBool("isRight"))
             {
                 ani.SetBool("isRight", false);
@@ -61,17 +69,28 @@ public class PlayerBoxing : MonoBehaviour
             ani.SetBool("isWalking",false);
         }
 
-        //J键出拳
-        if (Input.GetMouseButtonDown(0))
+        //J    拳
+        if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.F)) && !ani.GetCurrentAnimatorStateInfo(0).IsName("PlayerBoxing"))
         {
             ani.SetTrigger("Boxing");
-            //如果在判定击中距离内并且朝向正确则判定击中
+            //      卸    芯    诓  页     确   卸     
             if (Vector3.Magnitude(enemy.transform.position - transform.position) < boxingHitDistance && (((enemy.transform.position.x - transform.position.x < 0) && ani.GetBool("isRight")) || ((enemy.transform.position.x - transform.position.x >= 0) && (!ani.GetBool("isRight")))))
             {
                 enemy.GetComponent<EnemyBoxing>().currentLife -= damage;
+                if (HarperAudioSource != null && AudioController.Instance != null)
+                    AudioController.Instance.PlaySFXOnSource(HarperAudioSource, PunchHitClip, HitVolume);
+                else if (AudioController.Instance != null)
+                    AudioController.Instance.PlaySFXAtPosition(PunchMissClip, transform.position, MissVolume);
+            }
+            else
+            {
+                if (HarperAudioSource != null && AudioController.Instance != null)
+                    AudioController.Instance.PlaySFXOnSource(HarperAudioSource, PunchMissClip, MissVolume);
+                else if (AudioController.Instance != null)
+                    AudioController.Instance.PlaySFXAtPosition(PunchMissClip, transform.position, MissVolume);
             }
         }
-        //更新血条
+        //    血  
         playerLifeSlider.value = currentLife / maxLife;
     }
 }
